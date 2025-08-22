da42a4974e771748e00cb490cbb3dfc2ca6ba6a0
okwunduevans-cyber <okwunduevans@gmail.com>
2025-08-21 22:33:42 -0500

    Fix Gradle structure: root plugins + app module build config

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,10 @@
-cat > "/storage/emulated/0/Web App/Android App/settings.gradle.kts" <<'KTS'
 pluginManagement {
     repositories {
+        gradlePluginPortal()
         google()
         mavenCentral()
-        gradlePluginPortal()
-    }
-    // CI needs explicit versions
-    plugins {
-        id("com.android.application") version "8.5.2"
-        id("org.jetbrains.kotlin.android") version "1.9.24"
     }
 }
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -19,7 +12,5 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-
 rootProject.name = "Simonfxx"
 include(":app")
-KTS
