da73353850bb7c26cee533daf474e990970df69e
okwunduevans-cyber <okwunduevans@gmail.com>
2025-08-21 19:27:34 -0500

    CI: workflow + plugin versions; build with Gradle 8.7

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,25 @@
+cat > "/storage/emulated/0/Web App/Android App/settings.gradle.kts" <<'KTS'
 pluginManagement {
-  repositories {
-    google()
-    mavenCentral()
-    gradlePluginPortal()
-  }
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    // CI needs explicit versions
+    plugins {
+        id("com.android.application") version "8.5.2"
+        id("org.jetbrains.kotlin.android") version "1.9.24"
+    }
 }
+
 dependencyResolutionManagement {
-  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-  repositories {
-    google()
-    mavenCentral()
-  }
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
+
 rootProject.name = "Simonfxx"
 include(":app")
+KTS
